39e4ac71f1421fa54b8df420952c3ee0b1d1ec82
jaydeepchakraborty <jaydeep.chakraborty.1988@gmail.com>
2017-08-15 22:52:06 -0700

    Sentiment -DNN

--- tensorflow_9.py
+++ tensorflow_9.py
@@ -2,98 +2,60 @@
 #check deeplearning_2
 
 #imports
-import nltk
-import tensorflow as tf
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-import numpy as np
-import random
 from collections import Counter
-from nltk.corpus import stopwords
-from stop_words import get_stop_words
+import pickle
+import random
 import traceback
 
-#constants
-pos = "data/pos.txt"
-neg = "data/neg.txt"
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sklearn import cross_validation
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from stop_words import get_stop_words
 
-lemmatizer = WordNetLemmatizer()
-stopWords = set(stopwords.words('english'))
-hm_lines = 100000
+import numpy as np
+import tensorflow as tf
 
 
-def create_lexicon(pos,neg):
-    print("3")
-    lexicon = []
-    for f in [pos,neg]:
-        with open(f,'r', encoding="utf-8") as f:
-            contents = f.readlines()
-            for l in contents[:hm_lines]:
-                all_words = word_tokenize(l.lower())
-                lexicon += list(all_words)
-                
-                
-    lexicon = [lemmatizer.lemmatize(i) for i in lexicon]
-    filtered_lexicon = []
+def loadFile():
+    data =[]
+    target = []
+    with open("data/pos.txt",'r', encoding="utf-8") as f:
+        pos_contents = f.readlines()
+        pos_contents = [x.strip() for x in pos_contents]
+        t_val = [0,1]
+        pos_target = [t_val] * len(pos_contents)
+        
+    with open("data/neg.txt",'r', encoding="utf-8") as f:
+        neg_contents = f.readlines()
+        neg_contents = [x.strip() for x in neg_contents]
+        t_val = [1,0]
+        neg_target = [t_val] * len(neg_contents)
+
+
+    data = pos_contents + neg_contents
+    target = pos_target + neg_target
     
-    for w in lexicon:
-        if w not in stopWords:
-            filtered_lexicon.append(w) 
-    
-    print("4")
-    return filtered_lexicon
+    return data,target
 
+def preprocess(data):
+    tfidf_vectorizer = TfidfVectorizer(min_df=1, stop_words='english')
+    tfidf_data = tfidf_vectorizer.fit_transform(data)
+    #tfidf_data = TfidfTransformer(use_idf=True).fit_transform(data)
 
-def sample_handling(sample,lexicon,classification):
-    print("5")
-
-    featureset = []
-
-    with open(sample,'r', encoding="utf-8") as f:
-        contents = f.readlines()
-        for l in contents[:hm_lines]:
-            current_words = word_tokenize(l.lower())
-            current_words = [lemmatizer.lemmatize(i) for i in current_words]
-            features = np.zeros(len(lexicon))
-            for word in current_words:
-                if word.lower() in lexicon:
-                    index_value = lexicon.index(word.lower())
-                    features[index_value] += 1
-
-            features = list(features)
-            featureset.append([features,classification])
-    print("6")
-    return featureset
-
-def create_feature_sets_and_labels(pos,neg,test_size = 0.1):
-    print("2")
-    lexicon = create_lexicon(pos,neg)
-    features = []
-    features += sample_handling('pos.txt',lexicon,[1,0]) #for positive class is [1,0]
-    features += sample_handling('neg.txt',lexicon,[0,1])#for negative class is [0,1]
-    random.shuffle(features)
-    features = np.array(features)
-    
-    
-
-    testing_size = int(test_size*len(features))#10% of features
-
-    train_x = list(features[:,0][:-testing_size])# all the features up to testing_size 
-    train_y = list(features[:,1][:-testing_size])# all the labels up to testing_size 
-    test_x = list(features[:,0][-testing_size:])
-    test_y = list(features[:,1][-testing_size:])
-
-    print("7")
-    return train_x,train_y,test_x,test_y
+    return tfidf_data.toarray()
 
 try:
-    print("1")
-    train_x,train_y,test_x,test_y = create_feature_sets_and_labels(pos,neg)
-    print("8")
-    
-    #2 hidden layers, and each has 500 nodes.The number 500 needs not to be same.
-    n_nodes_hl1 = 500
-    n_nodes_hl2 = 500
+    data,target = loadFile()
+    tf_idf = preprocess(data)
+     
+    train_x,test_x,train_y,test_y = train_test_split(tf_idf,target,test_size=0.4,random_state=43,shuffle=True)
+
     
     n_classes = 2
     batch_size = 100#
@@ -137,7 +99,7 @@ try:
     #initialising variables
     init = tf.global_variables_initializer()
     sess.run(init)
-    hm_epochs = 1000
+    hm_epochs = 100
     
     print("9")
     for epoch in range(hm_epochs):
@@ -150,7 +112,7 @@ try:
             
             batch_x = np.array(train_x[start:end])
             batch_y = np.array(train_y[start:end])
-            _, c = sess.run([optimizer,cross_entropy], feed_dict = {x : batch_x, y : batch_y, pkeep:1})#c is cost
+            _, c = sess.run([optimizer,cross_entropy], feed_dict = {x : batch_x, y_ : batch_y, pkeep:1})#c is cost
             epoch_loss += c
             i +=  batch_size
         print('Epoch', epoch, 'completed out of', hm_epochs, 'loss', epoch_loss)
@@ -162,7 +124,7 @@ try:
     acc = sess.run(accuracy, feed_dict=feed_dict_test)
     
     print("Accuracy:- "+str(acc))
-    print("10")
+    
     
 except:
     traceback.print_exc()
